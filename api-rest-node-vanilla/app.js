1498b507aa03f1a5684f04916067daae2e11da9e
eymard <francisco.eymard.oliveira@gmail.com>
2023-01-13 23:51:34 -0300

    create rest api files

--- api-rest-node-vanilla/app.js
+++ api-rest-node-vanilla/app.js
@@ -4,13 +4,13 @@ const PORT = process.env.PORT || 5000
 
 const server = http.createServer(async (req,res) => {
     if (req.url === "/api" && req.method === "GET") {
-        // response headers
+
         res.writeHead(200, {
             'Content-Type': "application/json"
         })
-        //set the response
+     
         res.write("Hi there, This is a Vanilla Node.js API");
-        //end the response
+    
         res.end();
     }
 
