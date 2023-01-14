c8c98f39c9e57cdfa917b499ee7be698d6a679c2
eymard <francisco.eymard.oliveira@gmail.com>
2023-01-14 10:40:18 -0300

    implement get a todo

--- api-rest-node-vanilla/controller.js
+++ api-rest-node-vanilla/controller.js
@@ -4,6 +4,18 @@ class Controller {
     async getTodos() {
         return new Promise((resolve, _) => resolve(data))
     }
+
+    async getTodo(id) {
+        return new Promise((resolve, reject) => {
+            let todo = data.find((todo) => todo.id === parseInt(id))
+
+            if (todo) {
+                resolve(todo)
+            } else {
+                reject(`Todo with id ${id} not found`)
+            }
+        })
+    }
 }
 
 module.exports = Controller
